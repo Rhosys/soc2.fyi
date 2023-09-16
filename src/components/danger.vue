24e876d2ed3fe56a4eca3dab329d81ed270d99a0
Warren Parad <gpg@warrenparad.net>
2023-09-16 17:59:25 +0200

    Completely redo table to use new vue3 version.

--- src/components/danger.vue
+++ src/components/danger.vue
@@ -1,9 +1,7 @@
 <template>
-  <td>
-    <div class="d-flex justify-content-center">
-      <span class="fa-stack fa-1x d-flex align-items-center justify-content-center text-danger">
-        <i class="fa-solid fa-xmark fa-stack-1x" />
-      </span>
-    </div>
-  </td>
+  <div class="d-flex justify-content-center">
+    <span class="fa-stack fa-1x d-flex align-items-center justify-content-center text-danger">
+      <i class="fa-solid fa-xmark fa-stack-1x" />
+    </span>
+  </div>
 </template>
--- src/components/warning.vue
+++ src/components/warning.vue
@@ -1,9 +1,7 @@
 <template>
-  <td>
-    <div class="d-flex justify-content-center">
-      <span class="fa-stack fa-1x d-flex align-items-center justify-content-center text-warning">
-        <i class="fa-solid fa-circle-question fa-stack-1x" />
-      </span>
-    </div>
-  </td>
+  <div class="d-flex justify-content-center">
+    <span class="fa-stack fa-1x d-flex align-items-center justify-content-center text-warning">
+      <i class="fa-solid fa-circle-question fa-stack-1x" />
+    </span>
+  </div>
 </template>
