c4022039c60472ad66b8e26f5a60dd72ebe25234
Warren Parad <5056218+wparad@users.noreply.github.com>
2023-10-26 23:31:28 +0200

    Update home.vue
    Signed-off-by: Warren Parad <5056218+wparad@users.noreply.github.com>

--- src/components/home.vue
+++ src/components/home.vue
@@ -332,7 +332,7 @@
             </div>
             <div>
               <h2>A Guide to personal security</h2>
-              <p><a target="_blank" href="https://democrats.org/wp-content/uploads/2022/03/Device-and-Account-Security-Checklist.pdf">US DNC Security Checklist</a>
+              <p><a target="_blank" href="https://github.com/Rhosys/soc2.fyi/blob/main/src/Device-and-Account-Security-Checklist.pdf">US DNC Security Checklist</a>
                 <br>They strongly recommend anyone who works in politics, campaigns, or really anyone who has a device or an account on the internet, take these steps to secure them.
                 <br><br>
 
